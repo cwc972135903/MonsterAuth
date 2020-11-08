54884a86b3c6d3f2b0b7f3a2c6596882091b8eab
cwc <972135903@qq.com>
2020-11-08 15:12:24 +0800

    身份认证服务

---
+++ src/main/java/com/bzhi2/monster/MonsterApplication.java
@@ -0,0 +1,13 @@
+package com.bzhi2.monster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonsterApplication.class, args);
+    }
+
+}
